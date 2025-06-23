2b43fdfb0f69da3eeb145c53e0893e42d493999f
Yes Yes <titus.rohweder@freenet.de>
2025-06-23 11:46:13 +0200

    Added basic structure and comments.

---
+++ guess_the_number.hs
@@ -0,0 +1,21 @@
+main :: IO ()
+
+main = do
+
+    -- This text displays on loading the function.
+    putStrLn "Guess the number between 1 and 10."
+    putStrLn "Please enter a number:"
+
+    -- This generates a random number.
+    let randomNumber = 2
+
+    -- This allows the user to write a number into the console.
+    input <- getLine
+    let guessnumber = read input :: Int
+
+    -- This prints the number which the user wrote.
+    putStrLn $ "You entered: " ++ show guessnumber
+
+    -- This evaluates whether the number is right or wrong.
+    putStrLn $ "Wrong number."
+    putStrLn $ "Right number."
